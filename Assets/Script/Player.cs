550fd67efdc75593a28ba457f887eb1cb9c3e763
hphptop <hphptop@naver.com>
2023-11-16 03:21:22 +0900

    <Fix>: <bake 수정>
    Tutorial 씬(Professor_House) navmesh bake 바닥 제외 수정

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,42 +4,63 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //public GameObject _character;
     float moveSpeed;
+    float rotateSpeed;
+    Vector3 tmpDir;
 
     void Start()
     {
         moveSpeed = 5f;
+        rotateSpeed = 10f;
     }
 
     void Update()
     {
         if(Input.GetKey(KeyCode.UpArrow))
         {
+            tmpDir = transform.forward;
             float z = transform.position.z;
             z += Time.deltaTime * moveSpeed;
             transform.position = new Vector3(transform.position.x, transform.position.y, z);
+            // 임시 회전
+            tmpDir = Vector3.RotateTowards(transform.forward, tmpDir, Time.deltaTime * rotateSpeed, 0);
+            transform.rotation = Quaternion.LookRotation(tmpDir.normalized);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
+            tmpDir = -(transform.forward);
             float z = transform.position.z;
             z -= Time.deltaTime * moveSpeed;
             transform.position = new Vector3(transform.position.x, transform.position.y, z);
+            // 임시 회전
+            tmpDir = Vector3.RotateTowards(transform.forward, tmpDir, Time.deltaTime * rotateSpeed, 0);
+            transform.rotation = Quaternion.LookRotation(tmpDir.normalized);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            tmpDir = transform.right;
             float x = transform.position.x;
             x += Time.deltaTime * moveSpeed;
             transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            // 임시 회전
+            tmpDir = Vector3.RotateTowards(transform.forward, tmpDir, Time.deltaTime * rotateSpeed, 0);
+            transform.rotation = Quaternion.LookRotation(tmpDir.normalized);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            tmpDir = -(transform.right);
             float x = transform.position.x;
             x -= Time.deltaTime * moveSpeed;
             transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            // 임시 회전
+            tmpDir = Vector3.RotateTowards(transform.forward, tmpDir, Time.deltaTime * rotateSpeed, 0);
+            transform.rotation = Quaternion.LookRotation(tmpDir.normalized);
         }
 
+        
     }
 }
